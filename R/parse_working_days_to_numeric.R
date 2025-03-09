c7d4ee462562b78b8092b20468f6180ad8d27159
Radian-n <65278813+Radian-n@users.noreply.github.com>
2025-03-09 16:32:47 +1300

    parse_working_days_to_numeric(): Add roxygen documentation to function.

--- R/parse_working_days_to_numeric.R
+++ R/parse_working_days_to_numeric.R
@@ -1,3 +1,31 @@
+#' Converts a dataframe with strings containing 'working days' into numeric values.
+#'
+#' @param process_row_df A dataframe with a single row and the columns: `Service`, `Develop Only`, `Dev + Scan`, `Prints Add On`. This corresponds to the turnaround time table from The Black and White Box website.
+#'
+#' @returns A dataframe. The same as the input dataframe, but with the cells replaced by digits representing turnaround time in days.
+#' @export
+#'
+#' @examples
+#' c41_string <- tibble::tibble(
+#'   "Service" = "C-41",
+#'   "Develop Only" = "2 Working Days",
+#'   "Dev + Scan" = "5 Working Days",
+#'   "Prints Add On" = "+ 1 Working Days"
+#' )
+#' 
+#' c41_string
+#' # # A tibble: 1 × 4
+#' #   Service `Develop Only` `Dev + Scan`   `Prints Add On` 
+#' #   <chr>   <chr>          <chr>          <chr>           
+#' # 1 C-41    2 Working Days 5 Working Days + 1 Working Days
+#' 
+#' # Notice that `Prints Add On` result is `Dev + Scan` + 1
+#' parse_working_days_to_numeric(c41_string)
+#' # A tibble: 1 × 4
+#' #   Service `Develop Only` `Dev + Scan` `Prints Add On`
+#' #   <chr>            <dbl>        <dbl>           <dbl>
+#' # 1 C-41                 2            5               6
+#' 
 parse_working_days_to_numeric <- function(process_row_df) {
   # For C-41, B&W, ECN-2, ALT
   # NOT E-6
