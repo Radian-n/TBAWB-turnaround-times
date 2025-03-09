e4308c2f0f6f136f9b1bb71b9d8c1bad34b44121
Radian-n <65278813+Radian-n@users.noreply.github.com>
2025-03-09 15:52:41 +1300

    days_until_weekday(): Add roxygen documentation to function. Add line breaks inside function body"

--- R/days_until_weekday.R
+++ R/days_until_weekday.R
@@ -1,3 +1,30 @@
+#' Calculate the number of days until a certain weekday.
+#' 
+#' The days from `date_from` until the next occurance of `weekday`.
+#' 
+#' If `date_from` is the same weekday as `weekday`, then function will return 7 days rather than 0. See examples.
+#'
+#' @param weekday A string. One of the weekdays, e.g "Monday".
+#' @param date_from A date. The date from which to start calculating the number of days until `weekday`.
+#'
+#' @returns An integer. The number of days until `weekday`
+#' @export
+#'
+#' @examples
+#' monday <- lubridate::ymd("2025-03-10")
+#' weekdays(monday)
+#' # [1] "Monday"
+#' 
+#' days_until_weekday("Wednesday", date_from = monday)
+#' # [1] 2
+#' 
+#' # Function exludes the current day. For example, 7 days from a wednesday until the next Wednesday, not 0 days.
+#' wednesday <- lubridate::ymd("2025-03-12")
+#' weekdays(wednesday)
+#' # [1] "Wednesday"
+#' days_until_weekday("Wednesday", date_from = wednesday)
+#' # [1] 7
+#' 
 days_until_weekday <- function(weekday, date_from = lubridate::today(tzone = "Pacific/Auckland")) {
 
   # Get today's weekday name
@@ -5,10 +32,10 @@ days_until_weekday <- function(weekday, date_from = lubridate::today(tzone = "Pa
 
   # Get the next 7 weekdays from today.
   next_7_weekdays <- weekdays(c(1:7) + date_from)
-  
+
   # Get the number of weekdays from today until `weekday_string`
-  days_to_weekday_string <- match(weekday, next_7_weekdays)
+  days_until_wday <- match(weekday, next_7_weekdays)
 
-  return(days_to_weekday_string)
+  return(days_until_wday)
 
-} 
+}
