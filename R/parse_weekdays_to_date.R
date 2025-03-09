8c3d48323ac7684a80a95832df92ae19eabedcdb
Radian-n <65278813+Radian-n@users.noreply.github.com>
2025-03-09 15:51:41 +1300

    parse_weekdays_to_date(): add date argument to function so that tests can be defined

--- R/parse_weekdays_to_date.R
+++ R/parse_weekdays_to_date.R
@@ -1,6 +1,5 @@
-parse_weekdays_to_date <- function(process_row_df) {
-
-  today <- lubridate::today(tzone = "Pacific/Auckland")
+parse_weekdays_to_date <- function(process_row_df, date_today = lubridate::today(tzone = "Pacific/Auckland")) {
+  # To bs used only for E-6 row
   
   dev_only_weekday <- extract_weekday_from_string(process_row_df$`Develop Only`)
   dev_and_scan_weekday <- extract_weekday_from_string(process_row_df$`Dev + Scan`)
@@ -11,7 +10,7 @@ parse_weekdays_to_date <- function(process_row_df) {
   # For dev + scan, if the order comes in on wednesday, thursday or friday it won't be due until NEXT friday
   # This section handles if the order comes in on wednesday or thursday.
   # Since days_until_weekday() already excludes the current day, Friday handled implicitly
-  today_wday <- weekdays(today)
+  today_wday <- weekdays(date_today)
   if (today_wday %in% c("wednesday", "thursday")) {
     # Adding 3 days to the date_from parameter to get us past friday, and into next week.
     # This gets us days until friday from saturday or sunday. Then add the extra 3 days so it gets us
@@ -22,8 +21,8 @@ parse_weekdays_to_date <- function(process_row_df) {
     dev_and_scan_days <- days_until_weekday(dev_and_scan_weekday)
   }
 
-  dev_only_date <- today + dev_only_days
-  dev_and_scan_date <- today + dev_and_scan_days
+  dev_only_date <- date_today + dev_only_days
+  dev_and_scan_date <- date_today + dev_and_scan_days
   prints_add_on_date <- bizdays::offset(dev_and_scan_date, 1, "bawb_calendar") # 1 extra business day
 
   result_df <- tibble::tibble(
