a4613462ca936f72330c882372eccbe2620fb22b
Radian-n <65278813+Radian-n@users.noreply.github.com>
2025-03-10 13:17:04 +1300

    extract_weekday_from_string() Add roxygen documentation.

--- R/extract_weekday_from_string.R
+++ R/extract_weekday_from_string.R
@@ -1,3 +1,19 @@
+#' Extracts the weekday name from a string.
+#'
+#' @param string A single string, possibily containing a weekday.
+#'
+#' @returns A single string (if a weekday exists in the string), else NA.
+#' @export
+#'
+#' @examples
+#' extract_weekday_from_string("This should return 'Monday'")
+#' # [1] "Monday"
+#' 
+#' extract_weekday_from_string("This doesn't have a weekday")
+#' # [1] NA
+#' 
+#' extract_weekday_from_string("Case insensitive: thuRsdaY")
+#' # [1] "Thursday"
 extract_weekday_from_string <- function(string) {
 
   # Conver to lowercase to ensure matching not case-sensative
